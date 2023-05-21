5adf224ec0d9b668231c8654d08d1fd7fd4a45b6
Felipe Lustosa <felipelustosaa6@gmail.com>
2023-05-21 19:24:16 -0300

    finished components

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,12 @@ export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
       Feito com 💜 no NLW da{' '}
-      <a className="underline hover:text-gray-100" href="">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://rocketseat.com.br"
+      >
         Rocketseat
       </a>
     </div>
